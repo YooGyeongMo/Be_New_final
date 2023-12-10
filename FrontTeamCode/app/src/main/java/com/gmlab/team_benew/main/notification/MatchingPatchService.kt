180c0da0f450dd989f9f27c8eaa19235cda87655
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2023-12-11 01:06:09 +0900

    매칭 알람 동기로 묶고 비동기로 코루틴 작업 완 100퍼

--- FrontTeamCode/app/src/main/java/com/gmlab/team_benew/main/notification/MatchingPatchService.kt
+++ FrontTeamCode/app/src/main/java/com/gmlab/team_benew/main/notification/MatchingPatchService.kt
@@ -16,63 +16,72 @@ class MatchingPatchService {
         this.matchingAlarmsPatchView = matchingAlarmsPatchView
     }
 
-    fun acceptMatch(context: Context, senderId: Long, receiverId: Long) {
+    suspend fun acceptMatch(
+        context: Context,
+        senderId: Long,
+        receiverId: Long
+    ): Response<MatchingAlarmResponse> {
         val token = getTokenFromSharedPreferences(context)
         val bearerToken = "Bearer $token"
         val service = getRetrofit().create(MatchingAlarmsReadRetrofitInterface::class.java)
-        service.matchingAlarmsAccess(bearerToken, senderId, receiverId).enqueue(object: Callback<MatchingAlarmResponse> {
-            override fun onResponse(call: Call<MatchingAlarmResponse>, response: Response<MatchingAlarmResponse>) {
-                Log.d("NETWORK_SUCCESS_ALARMS_PATCH_수락","USER_MATCHING_PATCH_수락_네트워크성공")
-                when(response.code()){
-                    200 -> {
-                        matchingAlarmsPatchView.onMatchingAlarmsAccessSuccess()
-                    }
-                    401 -> {
-                        Log.e("NotificationPATCH/수락/ERROR", "401 ERROR ${response.code()}")
-                        matchingAlarmsPatchView.onMatchingAlarmsAccessFailure()
-                    }
-                    else -> {
-                        Log.e("NotificationPATCH/수락/ERROR", "ERROR ${response.code()}")
-                        matchingAlarmsPatchView.onMatchingAlarmsAccessFailure()
-                    }
-                }
-            }
-            override fun onFailure(call: Call<MatchingAlarmResponse>, t: Throwable) {
-                Log.e("NETWORK_SUCCESS_ALARMS_PATCH_수락","USER_MATCHING_PATCH_수락_네트워크성공",t)
-                matchingAlarmsPatchView.onMatchingAlarmsAccessFailure()
-            }
-        })
+        return service.matchingAlarmsAccess(bearerToken, senderId, receiverId)
+        //비동기 로직
+        //        service.matchingAlarmsAccess(bearerToken, senderId, receiverId).enqueue(object: Callback<MatchingAlarmResponse> {
+//            override fun onResponse(call: Call<MatchingAlarmResponse>, response: Response<MatchingAlarmResponse>) {
+//                Log.d("NETWORK_SUCCESS_ALARMS_PATCH_수락","USER_MATCHING_PATCH_수락_네트워크성공")
+//                when(response.code()){
+//                    200 -> {
+//                        matchingAlarmsPatchView.onMatchingAlarmsAccessSuccess()
+//                    }
+//                    401 -> {
+//                        Log.e("NotificationPATCH/수락/ERROR", "401 ERROR ${response.code()}")
+//                        matchingAlarmsPatchView.onMatchingAlarmsAccessFailure()
+//                    }
+//                    else -> {
+//                        Log.e("NotificationPATCH/수락/ERROR", "ERROR ${response.code()}")
+//                        matchingAlarmsPatchView.onMatchingAlarmsAccessFailure()
+//                    }
+//                }
+//            }
+//            override fun onFailure(call: Call<MatchingAlarmResponse>, t: Throwable) {
+//                Log.e("NETWORK_SUCCESS_ALARMS_PATCH_수락","USER_MATCHING_PATCH_수락_네트워크성공",t)
+//                matchingAlarmsPatchView.onMatchingAlarmsAccessFailure()
+//            }
+//        })
     }
 
-    fun rejectMatch(context: Context, senderId: Long, receiverId: Long) {
+    suspend fun rejectMatch(context: Context, senderId: Long, receiverId: Long): Response<MatchingAlarmResponse> {
         val token = getTokenFromSharedPreferences(context)
         val bearerToken = "Bearer $token"
         val service = getRetrofit().create(MatchingAlarmsReadRetrofitInterface::class.java)
-        service.matchingAlarmsReject(bearerToken, senderId, receiverId).enqueue(object:
-            Callback<MatchingAlarmResponse> {
-            override fun onResponse(call: Call<MatchingAlarmResponse>, response: Response<MatchingAlarmResponse>) {
-                Log.d("NETWORK_SUCCESS_ALARMS_PATCH_거절","USER_MATCHING_PATCH_거절_네트워크성공")
-                when(response.code()){
-                    200 -> {
-                        matchingAlarmsPatchView.onMatchingAlarmsRejectSuccess()
-                    }
-
-                    401 -> {
-                        Log.e("NotificationPATCH/거절/ERROR", "401 ERROR ${response.code()}")
-                        matchingAlarmsPatchView.onMatchingAlarmsRejectFailure()
-                    }
-
-                    else -> {
-                        Log.e("NotificationPATCH/거절/ERROR", "ERROR ${response.code()}")
-                        matchingAlarmsPatchView.onMatchingAlarmsRejectFailure()
-                    }
-                }
-            }
-            override fun onFailure(call: Call<MatchingAlarmResponse>, t: Throwable) {
-                Log.d("NETWORK_SUCCESS_ALARMS_PATCH_거절","USER_MATCHING_PATCH_거절_네트워크실패")
-                matchingAlarmsPatchView.onMatchingAlarmsRejectFailure()
-            }
-        })
+//동기로직
+        return service.matchingAlarmsReject(bearerToken, senderId, receiverId)
+        //비동기 로직
+        //        service.matchingAlarmsReject(bearerToken, senderId, receiverId).enqueue(object:
+//            Callback<MatchingAlarmResponse> {
+//            override fun onResponse(call: Call<MatchingAlarmResponse>, response: Response<MatchingAlarmResponse>) {
+//                Log.d("NETWORK_SUCCESS_ALARMS_PATCH_거절","USER_MATCHING_PATCH_거절_네트워크성공")
+//                when(response.code()){
+//                    200 -> {
+//                        matchingAlarmsPatchView.onMatchingAlarmsRejectSuccess()
+//                    }
+//
+//                    401 -> {
+//                        Log.e("NotificationPATCH/거절/ERROR", "401 ERROR ${response.code()}")
+//                        matchingAlarmsPatchView.onMatchingAlarmsRejectFailure()
+//                    }
+//
+//                    else -> {
+//                        Log.e("NotificationPATCH/거절/ERROR", "ERROR ${response.code()}")
+//                        matchingAlarmsPatchView.onMatchingAlarmsRejectFailure()
+//                    }
+//                }
+//            }
+//            override fun onFailure(call: Call<MatchingAlarmResponse>, t: Throwable) {
+//                Log.d("NETWORK_SUCCESS_ALARMS_PATCH_거절","USER_MATCHING_PATCH_거절_네트워크실패")
+//                matchingAlarmsPatchView.onMatchingAlarmsRejectFailure()
+//            }
+//        })
     }
 
     private fun getTokenFromSharedPreferences(context: Context): String? {
