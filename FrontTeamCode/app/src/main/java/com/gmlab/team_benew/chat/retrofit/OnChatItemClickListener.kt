490b3ba1036e9d820ae70645220b838b43f817ba
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2023-12-11 04:40:03 +0900

    90프로 완성

--- FrontTeamCode/app/src/main/java/com/gmlab/team_benew/chat/retrofit/OnChatItemClickListener.kt
+++
@@ -1,5 +0,0 @@
-package com.gmlab.team_benew.chat.retrofit
-
-interface OnChatItemClickListener {
-    fun onChatItemClick(roomId: String)
-}
---
+++ FrontTeamCode/app/src/main/java/com/gmlab/team_benew/main/notification/chattingpost/ChattingPostView.kt
@@ -0,0 +1,4 @@
+package com.gmlab.team_benew.main.notification.chattingpost
+
+interface chattingPostView {
+}
