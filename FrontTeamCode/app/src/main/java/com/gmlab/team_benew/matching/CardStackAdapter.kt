e203a7c31138f4d7b39b92c9476e28ef17a06b50
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2023-12-11 18:41:57 +0900

    100프로 구현완

--- FrontTeamCode/app/src/main/java/com/gmlab/team_benew/matching/CardStackAdapter.kt
+++ FrontTeamCode/app/src/main/java/com/gmlab/team_benew/matching/CardStackAdapter.kt
@@ -1,12 +1,14 @@
 package com.gmlab.team_benew.matching
 
 import android.content.Context
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.gmlab.team_benew.R
 
 class CardStackAdapter(val context: Context, items: List<Profile>) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>(){
@@ -39,9 +41,20 @@ class CardStackAdapter(val context: Context, items: List<Profile>) : RecyclerVie
         private val instructionTextView: TextView = itemView.findViewById(R.id.tv_matching_profile_instruction_data)
         private val peerReviewData: ImageView = itemView.findViewById((R.id.iv_matching_peerReview_data))
         private val projectExperienceTextView: TextView = itemView.findViewById(R.id.tv_matching_project_booleanCheck_data)
-//        private val profileImageView: ImageView = itemView.findViewById(R.id.civ_matching_profile_user_image)
+        private val profileImageView: ImageView = itemView.findViewById(R.id.civ_matching_profile_user_image)
 
         fun binding(profile: Profile){
+            // 프로필 이미지 로딩 (Glide 사용)
+            profile.photo?.let {
+                val imageByteArray = Base64.decode(it, Base64.DEFAULT)
+                Glide.with(context)
+                    .load(imageByteArray)
+                    .into(profileImageView)
+            } ?: run {
+                // 프로필 사진이 null일 경우 기본 이미지 설정
+                profileImageView.setImageResource(R.drawable.profile_main) // 예시로 기본 이미지를 설정
+            }
+
             nameTextView.text = profile.member.name
             roleTextView.text = profile.role
             instructionTextView.text = profile.instruction
