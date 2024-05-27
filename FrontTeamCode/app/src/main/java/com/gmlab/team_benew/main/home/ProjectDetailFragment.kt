47b1edab8238273a2b684dfe5e93c8cb1522dce4
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2024-05-27 23:07:51 +0900

    홈 레이아웃 수정

--- FrontTeamCode/app/src/main/java/com/gmlab/team_benew/main/home/ProjectDetailFragment.kt
+++ FrontTeamCode/app/src/main/java/com/gmlab/team_benew/main/home/ProjectDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.gmlab.team_benew.main.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,11 +9,28 @@ import androidx.fragment.app.Fragment
 import com.gmlab.team_benew.R
 
 class ProjectDetailFragment:Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_my_project_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //번들에서 projectId가져옴
+        val projectId = arguments?.getInt("projectId")
+
+        //project를 UI에 표시하거나 로그에 출력
+        if(projectId != null) {
+            // 로그에 출력
+            Log.d("ProjectDetailFragment", "Received projectId: $projectId")
+        }
+        else{
+            Log.e("ProjectDetailFragment", "No projectId")
+        }
+
     }
 }
